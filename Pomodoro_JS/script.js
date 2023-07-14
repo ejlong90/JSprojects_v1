45b693a9ba5ebd7365dbebc309a55458c26bdc3e
ejlong90 <ejlong21@gmail.com>
2023-07-14 17:00:06 -0400

    test 2

---
+++ Pomodoro_JS/script.js
@@ -0,0 +1,20 @@
+// variables
+
+let workTitle = document.getElementById("work");
+let breakTitle = document.getElementById("break");
+
+let workTime = 60;
+let breaktime = 10;
+
+let seconds = "00";
+
+// display
+window.onload = () => {
+  document.getElementById("minutes").innerHTML = workTime;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  workTitle.classList.add("active");
+};
+
+// start timer
+// testing
