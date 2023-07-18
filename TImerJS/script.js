6024f02c9c35cc4fff992155606b3196fb72d364
ejlong90 <ejlong21@gmail.com>
2023-07-18 11:43:35 -0400

    updating logic_seconds2

--- TImerJS/script.js
+++ TImerJS/script.js
@@ -7,20 +7,26 @@ function delay(milliseconds) {
   });
 }
 
-async function timer() {
+async function timer(status) {
+  console.log(status);
   while (minutes > 0) {
-    // console.log(minutes); // html element -- user will set the minutes
-    while (seconds > 0) {
-      // console.log(seconds); // html element -- user will set the seconds
-      document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+    console.log(minutes);
+    var myElement = document.getElementById("minutes");
+    myElement.innerHTML = minutes - 1; // html element -- user will set the minutes
+    while (seconds >= 0) {
+      console.log(seconds); // html element -- user will set the seconds
+      // document.getElementById("timer").value = minutes + ":" + seconds;
+      var myElement = document.getElementById("seconds");
+      myElement.innerHTML = seconds;
       await delay(1000);
       seconds--;
     }
     seconds = 60;
     minutes--;
   }
+  alert("Take a break!");
 }
 
-timer();
+// timer();
 
 // session length and break length
